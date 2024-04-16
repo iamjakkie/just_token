f09f66e965e882043c0a9cfaf10fc1a3aa5473cf
jakkie <jakubkielbasiewicz@gmail.com>
2024-04-16 22:18:56 +0200

    balanceof

--- test/Token.js
+++ test/Token.js
@@ -2,29 +2,38 @@ const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 describe("Token", () => {
+    const name = 'JUST Token';
+    const symbol = 'JUST';
+    const decimals = 18;
+    const totalSupply = ethers.utils.parseUnits('1000000', 18);
 
     let token;
+    let deployer;
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory("Token");
         token = await Token.deploy("JUST Token", "JUST", 1000000);
+        let accounts = await ethers.getSigners();
+        deployer = accounts[0].address;
     })
 
-
     it("Should return the right name", async () => {
-        expect(await token.name()).to.equal("JUST Token");
+        expect(await token.name()).to.equal(name);
     });
 
     it("Should return the right symbol", async () => {
-        expect(await token.symbol()).to.equal("JUST");
+        expect(await token.symbol()).to.equal(symbol);
     });
 
     it("Should return the right decimals", async () => {
-        expect(await token.decimals()).to.equal(18);
+        expect(await token.decimals()).to.equal(decimals);
     });
 
     it("Should return the right totalSupply", async () => {
-        const value = ethers.utils.parseUnits('1000000', 18);
-        expect(await token.totalSupply()).to.equal(value);
+        expect(await token.totalSupply()).to.equal(totalSupply);
     });
+
+    it("assigns total supply to deployer", async () => {
+        expect(await token.balanceOf(deployer)).to.equal(totalSupply);
+    })
 });
